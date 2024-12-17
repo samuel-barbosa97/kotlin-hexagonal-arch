12d0b2ec08b30ba48d69edd2478b87f23eb7a6ca
Samuel Barbosa <samuel.barbosa1997@hotmail.com>
2024-12-17 20:14:39 -0300

    Creating domain classes

---
+++ src/main/kotlin/com/barbosa/hexagonal/application/core/domain/Address.kt
@@ -0,0 +1,7 @@
+package com.barbosa.hexagonal.application.core.domain
+
+data class Address(
+    val street: String,
+    val city: String,
+    val state: String,
+)
---
+++ src/main/kotlin/com/barbosa/hexagonal/application/core/domain/Customer.kt
@@ -0,0 +1,9 @@
+package com.barbosa.hexagonal.application.core.domain
+
+data class Customer(
+    val id: String? = null,
+    val name: String,
+    var address: Address? = null,
+    val cpf: String,
+    val isValidCpf: Boolean = false
+)
