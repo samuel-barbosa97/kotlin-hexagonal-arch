24fe5f2f54293b873cf8bb46382693c38b8150e4
Samuel Barbosa <samuel.barbosa1997@hotmail.com>
2025-01-14 08:46:21 -0300

    Sending the CPF for validation upon change

--- src/main/kotlin/com/barbosa/hexagonal/application/core/usecase/UpdateCustomerUseCase.kt
+++ src/main/kotlin/com/barbosa/hexagonal/application/core/usecase/UpdateCustomerUseCase.kt
@@ -4,22 +4,27 @@ import com.barbosa.hexagonal.application.core.domain.Customer
 import com.barbosa.hexagonal.application.ports.`in`.FindCustomerByIdInputPort
 import com.barbosa.hexagonal.application.ports.`in`.UpdateCustomerInputPort
 import com.barbosa.hexagonal.application.ports.out.FindAddressByZipCodeOutputPort
+import com.barbosa.hexagonal.application.ports.out.SendCpfForValidationOutputPort
 import com.barbosa.hexagonal.application.ports.out.UpdateCustomerOutputPort
 
 class UpdateCustomerUseCase(
     private val findCustomerByIdInputPort: FindCustomerByIdInputPort,
     private val findAddressByZipCodeOutputPort: FindAddressByZipCodeOutputPort,
-    private val updateCustomerOutputPort: UpdateCustomerOutputPort
+    private val updateCustomerOutputPort: UpdateCustomerOutputPort,
+    private val sendCpfForValidationOutputPort: SendCpfForValidationOutputPort
 ): UpdateCustomerInputPort {
 
     override fun update(customer: Customer, zipCode: String) {
         if (customer.id == null) throw IllegalArgumentException("Customer ID cannot be null")
-        findCustomerByIdInputPort.find(customer.id)
+        val savedCpf = findCustomerByIdInputPort.find(customer.id).cpf
 
         customer.apply {
             address = findAddressByZipCodeOutputPort.find(zipCode)
         }.let {
             updateCustomerOutputPort.update(it)
+            if (savedCpf != it.cpf) {
+                sendCpfForValidationOutputPort.send(it.cpf)
+            }
         }
     }
 }
